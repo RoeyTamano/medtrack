5d710ed515a6ab0ecd45ed31d8bfd59571eb8503
jbt <noya.weinberg6340@gmail.com>
2024-09-18 15:02:08 +0300

    1

--- 123.py
+++ 123.py
@@ -1,16 +1,16 @@
-# import pandas as pd
-#
-# df = pd.read_csv("Drug.csv",
-#                  header=0,
-#                  usecols=["Drug", "Information", "Effective"])
-# drags = df.Drug.unique()
-# drags=list(drags)
-# print(drags)
-# drag_choice = input("enter ")
-# if drag_choice in drags:
-#     drag_ind=drags.index(drag_choice)
-#     print(df.loc[drag_ind+1])
-#     print()
+import pandas as pd
+
+df = pd.read_csv("Drug.csv",
+                 header=0,
+                 usecols=["Drug", "Information", "Effective"])
+drags = df.Drug.unique()
+drags=list(drags)
+print(drags)
+drag_choice = input("enter ")
+if drag_choice in drags:
+    drag_ind=drags.index(drag_choice)
+    print(df.loc[drag_ind+1])
+    print()
 
 
 
